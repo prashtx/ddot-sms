d151a95c710194e1e7095c10a2b1ef3745d1d78f
Prashant Singh <prashant@codeforamerica.org>
2012-06-22 16:19:00 -0700

    Use multiple geocoders
    Send simultaneous requests to Yahoo Placefinder and Nominatum, in case
    one fails or has poor data.

---
+++ geocoder-yahoo.js
@@ -0,0 +1,51 @@
+/*jslint node: true, indent: 2, sloppy: true, white: true, vars: true */
+
+/*
+ * Geocode functionality using the Yahoo Placefinder geocoder
+ */
+
+var request = require('request');
+var url = require('url');
+var Q = require('q');
+
+var apiUrl = 'http://where.yahooapis.com/geocode';
+var appId = process.env.YDN_KEY || '';
+var start = 0;
+var count = 1;
+var flags = 'J';
+
+module.exports = (function () {
+  var self = {};
+
+  self.code = function (line1, line2) {
+    var urlObj = url.parse(apiUrl);
+    urlObj.query = {
+      appid: appId,
+      start: start.toString(),
+      count: count.toString(),
+      flags: flags,
+      line1: line1,
+      line2: line2
+    };
+
+    var def = Q.defer();
+    request.get(url.format(urlObj), function(error, resp, body) {
+      if (error || resp.statusCode !== 200) {
+        def.reject();
+        return;
+      }
+
+      var data = JSON.parse(body);
+      var coords = {
+        lat: data.ResultSet.Results[0].latitude,
+        lon: data.ResultSet.Results[0].longitude,
+        meta: { quality: data.ResultSet.Results[0].quality }
+      };
+      def.resolve(coords);
+    });
+
+    return def.promise;
+  };
+
+  return self;
+}());
--- geocoder.js
+++ geocoder.js
@@ -1,45 +1,78 @@
 /*jslint node: true, indent: 2, sloppy: true, white: true, vars: true */
 
+/*
+ * Multi-service geocoder.
+ * Asynchronously calls Yahoo Placefinder and Nominatum. If Yahoo's quality
+ * indication is poor, we use Nominatum, unless the latter didn't return any
+ * results.
+ */
+
 var request = require('request');
 var url = require('url');
 var Q = require('q');
 
-var apiUrl = 'http://where.yahooapis.com/geocode';
-var appId = process.env.YDN_KEY || '';
-var start = 0;
-var count = 1;
-var flags = 'J';
+var yahoo = require('./geocoder-yahoo.js');
+var nominatum = require('./geocoder-nominatum.js');
+
+var minYahooQuality = 40;
 
 module.exports = (function () {
   var self = {};
 
   self.code = function (line1, line2) {
-    var urlObj = url.parse(apiUrl);
-    urlObj.query = {
-      appid: appId,
-      start: start.toString(),
-      count: count.toString(),
-      flags: flags,
-      line1: line1,
-      line2: line2
-    };
+    var yahooPromise = yahoo.code(line1, line2);
+    var nominatumPromise = nominatum.code(line1, line2);
 
-    var def = Q.defer();
-    request.get(url.format(urlObj), function(error, resp, body) {
-      if (error || resp.statusCode !== 200) {
-        def.reject();
-        return;
-      }
+    var yahooResult;
+    var yahooReason;
+    var nominatumResult;
+    var nominatumReason;
 
-      var data = JSON.parse(body);
-      var coords = {
-        lat: data.ResultSet.Results[0].latitude,
-        lon: data.ResultSet.Results[0].longitude
-      };
-      def.resolve(coords);
+    // TODO: If Yahoo Placefinder is successful and meets the quality bar, then
+    // we don't have to wait for the Nominatum request.
+    yahooPromise.then(function (value) {
+      yahooResult = value;
+    })
+    .fail(function (reason) {
+      console.log('Yahoo Placefinder failed: ' + reason.message);
+      yahooReason = reason;
     });
 
-    return def.promise;
+    nominatumPromise.then(function (value) {
+      nominatumResult = value;
+    })
+    .fail(function (reason) {
+      console.log('Nominatum failed: ' + reason.message);
+      nominatumReason = reason;
+    });
+
+    return Q.allResolved([yahooPromise, nominatumPromise])
+    .then(function (promises) {
+      if (nominatumReason && yahooReason) {
+        throw new Error('All geocoder services failed to return results.');
+      }
+
+      if (nominatumReason) {
+        // Nominatum failed, Yahoo succeeded, so Yahoo's quality value doesn't matter.
+        console.log('Using Yahoo Placefinder geocoder');
+        return yahooPromise;
+      }
+
+      if (yahooReason) {
+        // Yahoo failed, so go with Nominatum
+        console.log('Using Nominatum geocoder');
+        return nominatumPromise;
+      }
+
+      // Both methods succeeded, so check Yahoo's quality
+      if (yahooResult.meta.quality < minYahooQuality) {
+        console.log('Using Nominatum geocoder');
+        return nominatumPromise;
+      }
+
+      console.log('Using Yahoo Placefinder geocoder');
+      return yahooPromise;
+    });
   };
 
   return self;
