a86be14b11ac860d66e896535eadde045cb68f34
Prashant Singh <prashant@codeforamerica.org>
2012-08-27 17:11:33 -0700

    Tweak wording for multiple choice prompt

--- strings.js
+++ strings.js
@@ -5,12 +5,12 @@ module.exports = {
   GenericFailMessage: 'Sorry! I couldn\'t process that. Try something like:\nwoodward and mack',
   NoArrivals: 'Sorry, I don\'t see anything for that stop',
   ClosestStop: 'Closest stop: %s.',
-  CloseRoutes: 'I found active routes nearby. Send the letter you\'d like:',
+  CloseRoutes: 'Please send the letter of the bus you\'d like:',
   NoCloseRoutes: 'Sorry, I don\'t see any active routes nearby.',
   OtherCloseStops: 'Send letter for:',
   OtherCloseRoutes: 'Send letter for:',
   SingleStop: '@ %s\n%s', // Stop name, arrivals
   SingleStopWithSched: '@ %s, %s *scheduled', // Stop name, arrivals
   MiscWithSched: '%s *scheduled',
-  Option: '%s)%s' // Letter, meaning
+  Option: '%s) %s' // Letter, meaning
 };
