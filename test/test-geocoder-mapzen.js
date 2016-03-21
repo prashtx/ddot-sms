f28d7ad76e57f3754c796ec4b99d1b4034d3aa6a
Matt Hampel <matth@localdata.com>
2016-03-21 13:58:15 -0400

    Add mapzen geocoder

---
+++ test/test-geocoder-mapzen.js
@@ -0,0 +1,44 @@
+var should = require('should');
+var mapzen = require('../geocoder-mapzen');
+
+describe('mapzen', function(){
+  it('should code an address', function(done){
+    mapzen.code("8706 lyndon", "Detroit, MI")
+     .then(function(coords){
+      coords.should.have.property('lat');
+      coords.should.have.property('lon');
+      coords.should.have.property('meta');
+
+      (coords.meta.confidence).should.be.aboveOrEqual(0.87);
+      done();
+     }, function(reason){
+      should.not.exist(reason);
+      done();
+     })
+     .catch(done);
+  });
+
+  it('fail on no results', function(done){
+    mapzen.code("123 noidea", "Detroit, MI")
+     .then(function(coords){
+      should.not.exist(coords);
+      done();
+     }, function(reason){
+      should.exist(reason);
+      done();
+     })
+     .catch(done);
+  });
+
+  it('fail on a bad address', function(done){
+    mapzen.code("123 noidea", "Detroit, MI")
+     .then(function(coords){
+      should.not.exist(coords);
+      done();
+     }, function(reason){
+      should.exist(reason);
+      done();
+     })
+     .catch(done);
+  });
+});
